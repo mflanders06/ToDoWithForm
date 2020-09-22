310fee11cfb5697ed83114c00835106fbb2cf13d
MFlanders <mflanders06@gmail.com>
2020-09-22 10:21:46 -0600

    update for 9-21

---
+++ index.js
@@ -0,0 +1,6 @@
+function submitForm(){
+    let taskName = document.getElementById("task");
+    let taskDate = document.getElementById("date");
+    let taskNote = document.getElementById("taskNote");
+    console.log(taskName + ' ' + taskDate + ' ' + taskNote);
+}
